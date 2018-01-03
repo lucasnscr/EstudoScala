851f1c6e2d1cafbc46fd00fd812df1c8bf787194
LucasNascimento <lucas.nascimento.scr>
2018-01-03 11:24:13 -0200

    EstudoEscala
    Second Commit

--- src/main/scala/Sum.scala
+++ src/main/scala/Sum.scala
@@ -1,12 +1,15 @@
   class Sum {
 
-    Object
-    def main(args: Array[String]): Unit
-    =
-    {
-      try{
-        val el
+    object Main {
+      def main(args: Array[String]) {
+        try {
+          val elems = args map Integer.parseInt
+          println("The sum of my arguments is: " + elems.foldRight(0) (_ + _))
+        } catch {
+          case e: NumberFormatException =>
+            println("Usage: scala Main <n1> <n2> ... ")
+        }
       }
     }
 
-}
+  }
